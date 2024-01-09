64700ccdd1c42e57ea9741f36e159d4c7c3f5985
traswin <traswin.m@gmail.com>
2024-01-09 20:36:59 +0700

    true

--- challenge3/Main.java
+++ challenge3/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Feature toggle configuration
         FeatureToggle featureToggle = new FeatureToggle();
-        featureToggle.setEmailNotificationEnabled(false);
+        featureToggle.setEmailNotificationEnabled(true);
 
         // Initialize the NotificationService with feature toggle
         NotificationService notificationService = new NotificationService(featureToggle);
